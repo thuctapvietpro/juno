7c581af8cdc0a276d3c96d8cba0ca6223dcddd51
tuananh <kunkunmonlyly@gmail.com>
2018-07-17 11:45:34 +0700

    push chuc nang quan ly thanh vien

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,27 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::group(['namespace'=>'Admin'],function(){
+	Route::group(['prefix'=>'login','middleware'=>'CheckLogedIn'],function(){
+		Route::get('/','LoginController@getLogin');
+		Route::post('/','LoginController@postLogin');
+	});
+	Route::get('logout','HomeController@getLogout');
+	Route::group(['prefix'=>'admin','middleware'=>'CheckLogedOut'],function(){
+		Route::get('home','HomeController@getHome');
+		
+		//Account
+		Route::group(['prefix'=>'account'],function(){
+			Route::get('/','AccountController@getAccount');
+			
+			Route::get('add','AccountController@getAddAccount');
+			Route::post('add','AccountController@postAddAccount');
+
+			Route::get('edit/{id}','AccountController@getEditAccount');
+			Route::post('edit/{id}','AccountController@postEditAccount');
+
+			Route::get('delete/{id}','AccountController@getDeleteAccount');
+		});
+		
+	});
+});
