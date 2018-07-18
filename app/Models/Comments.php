40ea31d3f7054539c5de2abf6ed74b61c8d33624
Vu Thanh Tung <thanhtung9617@gmail.com>
2018-07-18 13:12:45 +0700

    push

--- app/Models/Comments.php
+++ app/Models/Comments.php
@@ -6,8 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comments extends Model
 {
-    //
+    
+    /**
+     * [$table description]
+     * @var string
+     */
     protected $table ='comments';
+    
+    /**
+     * [$primaryKey description]
+     * @var string
+     */
     protected $primaryKey='comment_id';
+    
+    /**
+     * [$guarded description]
+     * @var array
+     */
     protected $guarded=[];
 }
